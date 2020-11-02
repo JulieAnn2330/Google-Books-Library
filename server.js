391f3b24ed9f287fb2263e28a88a5f525db93007
Julie Schaub <julie.schaub@hotmail.com>
2020-11-02 12:06:14 -0600

    added server.js and dependencies

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require ("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+//const apiRoutes = require("./routes/api-routes");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+//app.use("/api", apiRoutes);
+
+// Connect to MongoDB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+
+mongoose.connect(
+  MONGODB_URI, 
+  { 
+    useNewUrlParser: true, 
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
