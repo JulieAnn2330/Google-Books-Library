e4d127e6f8c0b1d4a3c9b09261ca685c0b98c994
Julie Schaub <julie.schaub@hotmail.com>
2020-11-02 15:42:22 -0600

    updated api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,17 +4,17 @@ const booksController = require("../controllers/booksController");
 const db = require("../models");
 
 router.get("/books", (req, res) => {
-    axios
-        .get("https://www.googleapis.com/books/v1/volumes", { params: req.query })
-        .then(({ data: {items } }) => res.json(items))
-        .catch(err => res.status(422).json(err));
+  axios
+    .get("https://www.googleapis.com/books/v1/volumes", { params: req.query })
+    .then(({ data: { items } }) => res.json(items))
+    .catch(err => res.status(422).json(err));
 });
 
 router.route("/savedBooks")
-    .get(booksController.findAll)
-    .post(booksController.create)
+  .get(booksController.findAll)
+  .post(booksController.create)
 
 router.route("/savedBooks/:googleId")
-    .delete(booksController.remove);
+  .delete(booksController.remove);
 
 module.exports = router;
